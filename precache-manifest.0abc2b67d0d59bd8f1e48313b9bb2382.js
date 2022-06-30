105cb72d1e55e1b3020cabbc6871e2954d9b6397
BiswarupMukherjee1 <89087014+BiswarupMukherjee1@users.noreply.github.com>
2022-06-30 13:39:15 +0530

    Updates

---
+++ precache-manifest.0abc2b67d0d59bd8f1e48313b9bb2382.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710a3bad32b9b8e3af49ed1e9db2cf6c",
+    "url": "/Decentralized-Video-Hosting/index.html"
+  },
+  {
+    "revision": "32dcf4f95ed4a638cc10",
+    "url": "/Decentralized-Video-Hosting/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "bcb0121e2bcc1b1b968e",
+    "url": "/Decentralized-Video-Hosting/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "32dcf4f95ed4a638cc10",
+    "url": "/Decentralized-Video-Hosting/static/js/2.342e5e0c.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/Decentralized-Video-Hosting/static/js/2.342e5e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb0121e2bcc1b1b968e",
+    "url": "/Decentralized-Video-Hosting/static/js/main.57277a5b.chunk.js"
+  },
+  {
+    "revision": "27f86e3ac8b58fe304f3",
+    "url": "/Decentralized-Video-Hosting/static/js/runtime-main.4ef7451f.js"
+  },
+  {
+    "revision": "9e02a88e54fb14899a2ea3c60bf8fa35",
+    "url": "/Decentralized-Video-Hosting/static/media/dvideo.9e02a88e.png"
+  }
+]);
